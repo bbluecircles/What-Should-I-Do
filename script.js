446ae091f0aca00f26b6ed09e7e0d6277537baa2
James Luna <bbluecircles@gmail.com>
2021-06-06 03:27:34 -0700

    Create 'script.js', Add default activites list, Add custom activites object

---
+++ script.js
@@ -0,0 +1,44 @@
+// Default Activities List
+
+const Activities = [
+    "Go for a walk",
+    "Find a recipe to cook",
+    "Watch your favorite TV show",
+    "Read a book",
+    "Eat out at a resturant",
+    "Go for a run",
+    "Search for new music to listen to",
+    "Draw",
+    "Go bowling",
+    "Write a journal",
+    "Go to the park",
+    "Go to the gym",
+    "Ask someone out",
+    "Visit a family member",
+    "Try a boxing class",
+    "Go to a bar",
+    "Go on a hike",
+    "Try a yoga class",
+    "Get a massage",
+    "Try a dance class",
+    "Watch a movie",
+    "Watch a Youtube video",
+    "Watch a documentary",
+    "Go to the zoo",
+    "Practice my favorite sport",
+    "Try a jiu jitsui class",
+    "Go to a museum",
+    "Go to a amusement park"
+];
+
+// Custom activites 
+const customActivites = {
+    activities: [
+        {
+            activity: null, // activity
+            withPeople: false // include a friend option (will select random person from people list)
+        }
+    ], // activity list
+    people: [] // friends/family list 
+}
+
